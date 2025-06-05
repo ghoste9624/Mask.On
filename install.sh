934e2e13b43051a4a651360457fe304ec5118d10
ghoste9624 <jackspade44@outlook.com>
2025-06-05 17:00:12 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/princekrvert/mask.git 
+cd mask
+npm install 
+npm install axios
+npm install prompt-sync 
+npm audit fix --force 
+mv ~/mask.on/mask.on.sh ~/mask.on/mask
